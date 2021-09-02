af00874c8217a94098bf150242a168546564ee0e
immehadi7 <mehedy.adi7@gmail.com>
2021-09-02 18:58:32 +0600

    all done

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ let buttonid = () => {
    searchInput.value = '' ;
   /*  console.log(searchInputValue) */
 
+
+
+    /* catch the url */
   let url = `https://openlibrary.org/search.json?q=${searchInputValue} ` ;
   fetch(url)
     .then(res => res.json())
@@ -14,7 +17,16 @@ let buttonid = () => {
 
 let showData = docs => {
     //  console.log( docs);
+
+    /* total search result found */
+    const totalresult = docs.length
+      const resultCounts = document.getElementById('resultcount');
+            resultCounts.innerHTML = `Total Result found : ${totalresult}`;
+        
+            // console.log(totalresult); 
    
+
+
     const result = document.getElementById('resultShow');
     /* clear the old result */
     result.textContent = '' ;
@@ -30,10 +42,13 @@ let showData = docs => {
 
     /* forEach loop start */
     docs.forEach(doc => {
+      
+
         // console.log(doc)
         let div = document.createElement('div');
         div.classList.add('col')
-    
+
+        /* img part */
         let imgUrl = ` https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg` ;
         
         div.innerHTML = `        
@@ -47,33 +62,8 @@ let showData = docs => {
         </div>
       </div>
         ` ;
+        /* append with parents */
         result.appendChild(div);
     })
 
 }
-
-
-
-
-
-
-/* for( const doc of docs ){
-  // console.log(doc)
-  let div = document.createElement('div');
-  div.classList.add('col')
-
-  let imgUrl = ` https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg` ;
-  
-  div.innerHTML = `
-  <div class="card h-100">
-  <img src="${imgUrl}" class="card-img-top img-fluid" alt="...">
-  <div class="card-body">
-    <h5 class="card-title"> Author Name:${doc.author_name} </h5>
-    <h6 class="card-tittle" > Book Name: ${doc.title} </h6>
-    <h6 class="card-tittle" > First Publish Year: ${doc.first_publish_year} </h6>
-    <p class="card-text">Publisher: ${doc.publisher}</p>
-  </div>
-</div>
-  ` ;
-  result.appendChild(div);
-} */
