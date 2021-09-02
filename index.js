5453fbac8282c1908c243ca3bccd377afb5919fa
immehadi7 <mehedy.adi7@gmail.com>
2021-09-02 15:50:54 +0600

    for each implement

--- index.js
+++ index.js
@@ -15,10 +15,7 @@ let showData = docs => {
     // console.log(docs);
     const result = document.getElementById('resultShow');
 
-
-
-
-    for( const doc of docs ){
+    docs.forEach(doc => {
         // console.log(doc)
         let div = document.createElement('div');
         div.classList.add('col')
@@ -27,7 +24,7 @@ let showData = docs => {
         
         div.innerHTML = `
         <div class="card h-100">
-        <img src="${imgUrl}" class="card-img-top img-fluid" alt="...">
+        <img src="${imgUrl}" class="card-img-top img-fluid" width="300" height="250" alt="...">
         <div class="card-body">
           <h5 class="card-title"> Author Name:${doc.author_name} </h5>
           <h6 class="card-tittle" > Book Name: ${doc.title} </h6>
@@ -37,6 +34,27 @@ let showData = docs => {
       </div>
         ` ;
         result.appendChild(div);
-    }
+    })
 
 }
+
+/* for( const doc of docs ){
+  // console.log(doc)
+  let div = document.createElement('div');
+  div.classList.add('col')
+
+  let imgUrl = ` https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg` ;
+  
+  div.innerHTML = `
+  <div class="card h-100">
+  <img src="${imgUrl}" class="card-img-top img-fluid" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"> Author Name:${doc.author_name} </h5>
+    <h6 class="card-tittle" > Book Name: ${doc.title} </h6>
+    <h6 class="card-tittle" > First Publish Year: ${doc.first_publish_year} </h6>
+    <p class="card-text">Publisher: ${doc.publisher}</p>
+  </div>
+</div>
+  ` ;
+  result.appendChild(div);
+} */
