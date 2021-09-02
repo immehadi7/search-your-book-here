c1f32500e453caf694d8c24c5f41a70c47990f0b
immehadi7 <mehedy.adi7@gmail.com>
2021-09-02 17:18:13 +0600

    no result found part

--- index.js
+++ index.js
@@ -9,12 +9,26 @@ let buttonid = () => {
   fetch(url)
     .then(res => res.json())
     .then(data => showData(data.docs))
+    
 }
 
 let showData = docs => {
-    // console.log(docs);
+    //  console.log( docs);
+   
     const result = document.getElementById('resultShow');
+    /* clear the old result */
+    result.textContent = '' ;
+    
 
+/*      No result found part  */
+   let resultError = document.getElementById('noResult') 
+    if(result !== -1){
+      resultError.innerText = "No result Found" ;
+    }
+
+
+
+    /* forEach loop start */
     docs.forEach(doc => {
         // console.log(doc)
         let div = document.createElement('div');
@@ -22,7 +36,7 @@ let showData = docs => {
     
         let imgUrl = ` https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg` ;
         
-        div.innerHTML = `
+        div.innerHTML = `        
         <div class="card h-100">
         <img src="${imgUrl}" class="card-img-top img-fluid" width="300" height="250" alt="...">
         <div class="card-body">
@@ -37,6 +51,11 @@ let showData = docs => {
     })
 
 }
+
+
+
+
+
 
 /* for( const doc of docs ){
   // console.log(doc)
