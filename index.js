f126ea50661878572ca454641bb265b2c9ea324e
immehadi7 <mehedy.adi7@gmail.com>
2021-09-02 15:35:25 +0600

    pic show done

--- index.js
+++ index.js
@@ -14,19 +14,25 @@ let buttonid = () => {
 let showData = docs => {
     // console.log(docs);
     const result = document.getElementById('resultShow');
-   
+
+
+
+
     for( const doc of docs ){
-        console.log(doc)
+        // console.log(doc)
         let div = document.createElement('div');
         div.classList.add('col')
+    
+        let imgUrl = ` https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg` ;
+        
         div.innerHTML = `
         <div class="card h-100">
-        <img src="" class="card-img-top" alt="...">
+        <img src="${imgUrl}" class="card-img-top img-fluid" alt="...">
         <div class="card-body">
           <h5 class="card-title"> Author Name:${doc.author_name} </h5>
           <h6 class="card-tittle" > Book Name: ${doc.title} </h6>
           <h6 class="card-tittle" > First Publish Year: ${doc.first_publish_year} </h6>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text">Publisher: ${doc.publisher}</p>
         </div>
       </div>
         ` ;
